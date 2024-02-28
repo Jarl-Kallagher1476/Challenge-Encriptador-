cb81ead4465f2469ada6bd6df146583b87ba8f59
Jarl Kallagher <72108963+Jarl-Alberto1476@users.noreply.github.com>
2024-02-28 00:08:22 -0600

    Logica terminada falta diseño

---
+++ js/app.js
@@ -0,0 +1,65 @@
+//Variables
+const campoTexto = document.querySelector('#campo-mensaje ')
+const campoMensaje = document.querySelector('#texto-encriptado')
+const botonLimpiar = document.querySelector('#limpiar');
+
+
+//Funciones
+
+const matriz = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat'],
+]
+
+function btnEncriptar() {
+    const texto = encriptar(campoTexto.value)
+    campoMensaje.value = texto
+}
+
+function btnDesencriptar() { 
+    const texto = desencriptar(campoTexto.value)
+    campoMensaje.value = texto
+}
+
+function encriptar(txt) {
+    for (let i = 0; i < matriz.length; i++) {
+        if (txt.includes(matriz[i][0])) {
+            txt = txt.replaceAll(
+                matriz[i][0],
+                matriz[i][1]
+            )
+        }
+    }
+    return txt
+}
+
+function desencriptar(txt ) {
+    for (let i = 0; i < matriz.length; i++) {
+        if (txt.includes(matriz[i][1])) {
+            txt = txt.replaceAll(
+                matriz[i][1],
+                matriz[i][0]
+            )
+        }
+    }
+    return txt
+}
+//Copiar Codigo del textarea 
+function copiarCodigo() {
+    const codigoTextarea = document.getElementById('texto-encriptado');
+    codigoTextarea.select();
+    document.execCommand('copy');
+    alert('Código copiado al portapapeles.');
+}
+
+//limpiar el textarea
+function limpiar() {
+    campoTexto.value = ''
+    // Verificar si campoTexto está vacío y limpiar campoMensaje si es así
+    if (campoTexto.value === '') {
+        campoMensaje.value = '';
+    }
+}
